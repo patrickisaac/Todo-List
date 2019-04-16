ad09433fea32c7901504a48fea777721ceaf637a
Patrick Isaac <patrick.isaac04@gmail.com>
2019-04-16 14:04:57 -0700

    remove button deletes individual items but only 1st index in array

--- script.js
+++ script.js
@@ -8,21 +8,17 @@ window.addEventListener('load', function() {
   }
 })
 
-window.addEventListener('click', function() {
-  let removeButton = document.querySelector('.remove__item')
-  removeButton.onclick = (event) => {
-    event.preventDefault()
-    const listContent = document.querySelector('.list__content')
-    const item = document.querySelector('.item').value
-    listContent.parentNode.remove(addItem(item))
-  }
-})
 
 const addItem = (value) => {
   const newItem = document.createElement('li')
   newItem.innerHTML = `
   <h4>${value}</h4>
-  <button class='remove__item'>Task Complete</button>
+  <button onclick='removeItem()' class='remove__item'>Task Complete</button>
   `
   return newItem
+}
+
+const removeItem = () => {
+  const listContent = document.querySelector('.list__content')
+  listContent.removeChild(listContent.childNodes[0])
 }
