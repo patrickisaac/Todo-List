ee9dfc61cd96f32a150592c752a8a3055b9cb516
Patrick Isaac <patrick.isaac04@gmail.com>
2019-04-16 20:33:21 -0700

    refactored code - used onclick methods

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-window.addEventListener('load', function() {
-  let addButton = document.querySelector('.add__item')
-  addButton.onclick = (event) => {
-    event.preventDefault()
-    const listContent = document.querySelector('.list__content')
-    const item = document.querySelector('.item').value
-    listContent.appendChild(addItem(item))
-    listContent.insertBefore(addItem, item)
-  }
-})
+const addItem = () => {
+  const listContent = document.querySelector('.list__content')
+  const item = document.querySelector('.item').value
 
-const addItem = (value) => {
-  document.querySelector('.item').value = ''
   const newItem = document.createElement('li')
   newItem.innerHTML = `
-  <h4>${value}</h4>
+  <h4>${item}</h4>
   <button onclick='removeItem()' class='remove__item'>Task Complete</button>
   `
-  return newItem
+  listContent.appendChild(newItem)
+
+  clearText()
 }
 
 const removeItem = () => {
   const item = event.target.parentNode
   const parent = item.parentNode
   parent.removeChild(item)
+}
+
+const clearText = () => {
+  document.querySelector('.item').value = ''
 }
